1f53d406430ad8e40bbd797f760d3257b21c3dc7
Ed Elliott <goeddieuk@gmail.com>
2015-08-20 17:27:13 +0100

    adding 2 versions of lottery code, single connection and multiple connections

--- src/ResponseTimeLottery/ResponseTimeLottery/Program.cs
+++ src/ResponseTimeLottery/ResponseTimeLottery/Program.cs
@@ -13,7 +13,13 @@ namespace ResponseTimeLottery
 
         private static void Main()
         {
-            //QUERIES
+            //MarsConnection();
+            MultipleConnections();
+        }
+
+        private static void MarsConnection(){
+
+        //QUERIES
 
             var query1 = "SELECT 1980 --the year that god made me";
             //var query2 = "WAITFOR DELAY '0:0:5'";
@@ -26,14 +32,15 @@ namespace ResponseTimeLottery
             {
                 //Create MARS connection  
                 var connection = new SqlConnection("Server=.;Integrated Security=SSPI;MultipleActiveResultSets=true;");
+                
                 connection.Open();
-
+                
                 QueryCompletions = 0;
 
                 //Run queries on seperate threads
                 Task.Factory.StartNew(() => { RunQuery(connection, query2); }, TaskCreationOptions.LongRunning);
                 Task.Factory.StartNew(() => { RunQuery(connection, query1); }, TaskCreationOptions.LongRunning);
-
+                
                 lock (_lock)
                     Console.WriteLine("Tasks Scheduled...");
 
@@ -42,6 +49,48 @@ namespace ResponseTimeLottery
                     Thread.Sleep(100);
 
                 connection.Close();
+            }
+        }
+
+        private static void MultipleConnections()
+        {
+            //QUERIES
+
+            var query1 = "SELECT 1980 --the year that god made me";
+            //var query2 = "WAITFOR DELAY '0:0:5'";
+            var query2 =
+                "select 1000; select top 1000 * from sys.sysindexes cross join sys.sysobjects;create table #ddd(id int); drop table #ddd";
+
+            _lock = new object();
+
+            while (true)
+            {
+                //Create MARS connection  
+                
+                var connection1 = new SqlConnection("Server=.;Integrated Security=SSPI;MultipleActiveResultSets=true;");
+                var connection2 = new SqlConnection("Server=.;Integrated Security=SSPI;MultipleActiveResultSets=true;");
+
+                
+                connection1.Open();
+                connection2.Open();
+
+                QueryCompletions = 0;
+
+                //Run queries on seperate threads
+                Task.Factory.StartNew(() => { RunQuery(connection1, query2); }, TaskCreationOptions.LongRunning);
+                Task.Factory.StartNew(() => { RunQuery(connection2, query1); }, TaskCreationOptions.LongRunning);
+
+
+                lock (_lock)
+                    Console.WriteLine("Tasks Scheduled...");
+
+                //Wait for them both to finish
+                while (QueryCompletions < 2)
+                    Thread.Sleep(100);
+
+                
+                connection1.Close();
+                connection2.Close();
             }
         }
 
@@ -62,7 +111,7 @@ namespace ResponseTimeLottery
 
             lock (_lock)
             {
-                if (timer.ElapsedMilliseconds > 10)
+                if (timer.ElapsedMilliseconds >= 10)
                     Console.ForegroundColor = ConsoleColor.Red;
 
                 if (timer.ElapsedMilliseconds < 10)
