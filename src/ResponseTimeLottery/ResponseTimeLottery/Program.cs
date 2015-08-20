787553927455fb2d34081240447d4c5c42f5fa17
Ed Elliott <goeddieuk@gmail.com>
2015-08-20 16:12:23 +0100

    Lottery

---
+++ src/ResponseTimeLottery/ResponseTimeLottery/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ResponseTimeLottery
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var query1 = "SELECT 1980 --the year that god made me";
+            var query2 = "WAITFOR DELAY '0:0:5'";
+            
+
+            while (true)
+            {
+                var connection = new SqlConnection("Server=.;Integrated Security=SSPI;MultipleActiveResultSets=true;");
+                connection.Open();
+
+                Task.Factory.StartNew(() => { RunQuery(connection, query1); }, TaskCreationOptions.LongRunning);
+                Task.Factory.StartNew(() => { RunQuery(connection, query2); }, TaskCreationOptions.LongRunning);
+
+                Console.WriteLine("Tasks Scheduled...");
+                Console.ReadLine();
+                connection.Close();
+            }
+        }
+
+
+        static void RunQuery(SqlConnection connection, string query)
+        {
+
+            Console.WriteLine("Running Query: {0}", query);
+            var timer = StartTimer();
+
+            var cmd = connection.CreateCommand();
+            cmd.CommandText = query;
+            var reader = cmd.ExecuteReader();
+
+            while (reader.Read());
+        
+            timer.Stop();
+
+            Console.WriteLine("Query: {0} took: {1} ms", query, timer.ElapsedMilliseconds);
+
+        }
+
+        private static Stopwatch StartTimer()
+        {
+            var sw = new Stopwatch();
+            sw.Start();
+            return sw;
+        }
+
+        
+    }
+}
